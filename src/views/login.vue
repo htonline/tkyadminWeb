d621bc9c14121d2861add28becc37b085af50106
wuxiaoxuan <wuxiaoxuan@163.com>
2022-06-07 20:12:12 +0800

    修改默认密码

--- src/views/login.vue
+++ src/views/login.vue
@@ -66,8 +66,8 @@ export default {
       codeUrl: '',
       cookiePass: '',
       loginForm: {
-        username: 'admin',
-        password: '123456',
+        username: '',
+        password: '',
         rememberMe: false,
         code: '',
         uuid: ''
