8fb32532c66e349f8f10da8b5103d6849fc585a0
Waves Zuo <986344867@qq.com>
2023-09-12 15:13:47 +0800

    雷达图片管理 部署成功

--- src/store/modules/api.js
+++ src/store/modules/api.js
@@ -22,13 +22,14 @@ const api = {
     fileUploadApi: baseUrl + '/api/localStorage',
     // radarPicture图片上传
     pictureUploadApi: baseUrl + '/api/radarPicture/uploadPicture',
-    // 雷达采集文件上传
-    radarFileUploadApi: baseUrl + '/api/radarAcquisitionUpload/uploadFile',
     // baseUrl: http://localhost:8014/
     baseApi: baseUrl,
     // 预览图请求地址,查找图片的时候：本地调试用这个，找的是自己计算机上的路径，部署到服务器上时，要改成服务器ip
-    // pictureBaseApi: 'http://120.46.140.233:8001'
-    pictureBaseApi: 'http://localhost:8001'
+    pictureBaseApi: 'http://120.46.140.233:8001',
+    // pictureBaseApi: 'http://localhost:8001',
+
+    // 雷达采集文件上传
+    radarFileUploadApi: baseUrl + '/api/radarAcquisitionUpload/uploadFile'
   }
 }
 
