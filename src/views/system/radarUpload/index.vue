8499dea61e63eb5cdcb7a41a59805bd4e0b78611
Waves Zuo <986344867@qq.com>
2023-10-13 10:11:49 +0800

    文件上传页面调整

---
+++ src/views/system/radarUpload/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <a-button @click="uploadFile"> 资源上传 </a-button>
+    <a-drawer
+      title="资源上传"
+      placement="right"
+      width="640"
+      :closable="false"
+      :visible="visible"
+      @close="onClose"
+    >
+      <upload></upload>
+    </a-drawer>
+  </div>
+</template>
+
+<script>
+import Upload from '@/components/Uploader/Index'
+export default {
+  name: 'WelcomePage',
+  data() {
+    return {
+      visible: true
+    }
+  },
+  components: {
+    Upload
+  },
+  methods: {
+    uploadFile() {
+      this.visible = true
+    },
+    onClose() {
+      this.visible = false
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
